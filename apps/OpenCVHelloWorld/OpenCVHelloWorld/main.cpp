c1fc07dc4e62e48ec419816819e77f8026b9cbf7
shaobohou <shaobohou@gmail.com>
2011-12-16 00:51:46 +0000

    added an OpenCV HelloWorld program

---
+++ apps/OpenCVHelloWorld/OpenCVHelloWorld/main.cpp
@@ -0,0 +1,24 @@
+// OpenCV_Helloworld.cpp : Defines the entry point for the console application.
+// Created for build/install tutorial, Microsoft Visual C++ 2010 Express and OpenCV 2.1.0
+
+//#include "stdafx.h"
+
+#include <Windows.h>
+
+#include <cv.h>
+#include <cxcore.h>
+#include <highgui.h>
+
+//int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char *argv[])
+{
+        IplImage *img = cvLoadImage("lena.png");
+        cvNamedWindow("Image:",1);
+        cvShowImage("Image:",img);
+
+        cvWaitKey();
+        cvDestroyWindow("Image:");
+        cvReleaseImage(&img);
+
+        return 0;
+}
