885acbb6c06e519a0db6efe0e7d444e300fa0ac2
shaobohou <shaobohou@gmail.com>
2012-01-23 08:15:17 +0000

    Reorganised code, moved separate classes into their own header files. Annotation tool now calls an open file dialog to choose an image file. The saved annotations now includes the corners of the room.

---
+++ apps/OpenCVHelloWorld/OpenCVHelloWorld/VanishingPoint.h
@@ -0,0 +1,118 @@
+#ifndef VANISHING_POINT_H
+#define VANISHING_POINT_H
+
+
+#include "Geometry.h"
+
+#include <fstream>
+#include <iostream>
+
+
+// vanishing point
+class VanishingPoint : public Selectable
+{
+public:
+    VanishingPoint() : m_point(new Point2D()), m_atInfinity(false) {}
+    VanishingPoint(const LineSegment &line) : m_point(new Point2D()), m_atInfinity(false)
+    {
+        addLine(line);
+    }
+
+    ~VanishingPoint()
+    {
+        for(unsigned int i = 0; i < lines.size(); i++)
+        {
+            delete lines[i];
+        }
+    }
+
+    // stub function
+    int selectionDistance(int x, int y)
+    {
+        return std::numeric_limits<int>::max();
+    }
+
+    // stub function
+    void update(int x, int y)
+    {
+        assert(lines.size()> 1);
+
+        // update vanishing point
+        m_atInfinity = intersectInfiniteLines(*lines[0], *lines[1], *m_point);
+    }
+
+    void registerCascade(SelectableGroup &selectables)
+    {
+        for(unsigned int i = 0; i < lines.size(); i++)
+        {
+            selectables.registerSelectable(this, lines[i]);
+            lines[i]->registerCascade(selectables);
+        }
+    }
+
+    void addLine(const LineSegment &line)
+    {
+        lines.push_back(new LineSegment(line));
+    }
+
+    Point2D* point() const
+    {
+        return m_point;
+    }
+
+    bool atInfinity() const
+    {
+        return m_atInfinity;
+    }
+
+    void render(IplImage *temp, const CvScalar &col) const
+    {
+        for(unsigned int i = 0; i < lines.size(); i++)
+        {
+            lines[i]->render(temp, col, 2);
+        }
+
+        if(lines.size() > 1)
+        {
+            if(!m_atInfinity)
+            {
+                cvLine(temp, cvPoint(*m_point), cvPoint(*m_point), col, 10);
+            }
+        }
+    }
+
+    void save(std::ofstream &out) const
+    {
+        out << lines.size() << std::endl;
+        for(unsigned int i = 0; i < lines.size(); i++)
+        {
+            out << lines[i]->beg().x() << " " << lines[i]->beg().y() << "  ";
+            out << lines[i]->end().x() << " " << lines[i]->end().y() << std::endl;;
+        }
+    }
+
+    void load(std::ifstream &in)
+    {
+        int nlines = 0;
+        in >> nlines;
+
+        lines.clear();
+        for(int i = 0; i < nlines; i++)
+        {
+            int bx, by, ex, ey;
+            in >> bx >> by;
+            in >> ex >> ey;
+            addLine(LineSegment(Point2D(bx, by), Point2D(ex, ey)));
+        }
+
+        update(0, 0);
+    }
+
+private:
+    Point2D *m_point;
+    bool m_atInfinity;
+    std::vector<LineSegment*> lines;
+};
+
+
+#endif
