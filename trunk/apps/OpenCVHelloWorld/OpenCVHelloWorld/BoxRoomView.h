9ee7952ab0c061fcb47eec34997b8eeaf058af16
shaobohou@gmail.com <shaobohou@gmail.com@c06704c2-2886-2df8-2b27-5efbbc2a7cc3>
2012-02-02 10:38:20 +0000

    Vanishing points are now stored within BoxRoomView

--- trunk/apps/OpenCVHelloWorld/OpenCVHelloWorld/BoxRoomView.h
+++ trunk/apps/OpenCVHelloWorld/OpenCVHelloWorld/BoxRoomView.h
@@ -5,19 +5,60 @@
 #include "VanishingPoint.h"
 
 
-// Wall Boundary
+// projective view of a box room defined by three vanishing points
 class BoxRoomView : public Selectable
 {
 public:
-    BoxRoomView() : m_handles(new Point2D[4]), m_vpoint1(0), m_vpoint2(0), m_vpoint3(0) {}
+    BoxRoomView() : m_vpoint1(0), m_vpoint2(0), m_vpoint3(0), m_handles(new Point2D[4]) {}
 
     ~BoxRoomView()
     {
         delete [] m_handles;
+        if(m_vpoint1) delete m_vpoint1;
+        if(m_vpoint2) delete m_vpoint2;
+        if(m_vpoint3) delete m_vpoint3;
+    }
+
+    // has all three vanishing points been initialised
+    int numberOfVanishingPoints() const
+    {
+        int count = 0;
+        if(m_vpoint1) ++count;
+        if(m_vpoint2) ++count;
+        if(m_vpoint3) ++count;
+
+        return count;
+    }
+
+    // has all three vanishing points been initialised
+    bool initialisedEnoughVanishingPoints() const
+    {
+        return numberOfVanishingPoints() >= 3;
+    }
+
+    // initialise a new vanishing point if more needed
+    VanishingPoint* initialiseNewVanishingPoint(const LineSegment &line)
+    {
+        // find an uninitialised vanishing point
+        VanishingPoint **vpoint = 0;
+        if(!m_vpoint1) vpoint = &m_vpoint1;
+        if(!m_vpoint2) vpoint = &m_vpoint2;
+        if(!m_vpoint3) vpoint = &m_vpoint3;
+
+        // initialise a new vanishing point
+        if(vpoint)
+        {
+            *vpoint = new VanishingPoint(line);
+            (*vpoint)->addLine(LineSegment(Point2D(line.beg())+Point2D(10, 10), Point2D(line.end())+Point2D(10, 10)));
+
+            return *vpoint;
+        }
+        
+        return 0;
     }
 
     // setup function
-    void setup(const Point2D &first, const Point2D &second, const std::vector<VanishingPoint*> &vanishings, const int width, const int height)
+    void setup(const Point2D &first, const Point2D &second, const Point2D &centre)
     {
         // compute the bounding box
         const Point2D::Real Xmin = std::min(first.x(), second.x());
@@ -32,35 +73,29 @@ public:
         m_handles[3].set(Point2D(Xmin, (Ymin+Ymax)/2));
 
         // call the real setup function
-        setup(vanishings, width, height);
+        setup(centre);
     }
 
     // setup function
-    void setup(const std::vector<VanishingPoint*> &vanishings, const int width, const int height)
+    void setup(const Point2D &centre)
     {
         // compute distances of vanishing points to the centre of the image
-        const Point2D::Real dist0 =  Point2D(vanishings[0]->point()).dist2(Point2D(width/2, height/2));
-        const Point2D::Real dist1 =  Point2D(vanishings[1]->point()).dist2(Point2D(width/2, height/2));
-        const Point2D::Real dist2 =  Point2D(vanishings[2]->point()).dist2(Point2D(width/2, height/2));
+        const Point2D::Real dist0 =  Point2D(m_vpoint1->point()).dist2(centre);
+        const Point2D::Real dist1 =  Point2D(m_vpoint2->point()).dist2(centre);
+        const Point2D::Real dist2 =  Point2D(m_vpoint3->point()).dist2(centre);
 
         // choose the closest vanishing point to construct the back wall
         if(dist0 <= std::min(dist1, dist2))
         {
-            m_vpoint1 = vanishings[1];
-            m_vpoint2 = vanishings[2];
-            m_vpoint3 = vanishings[0];
+            std::swap(m_vpoint1, m_vpoint3);
         }
         else if(dist1 <= std::min(dist0, dist2))
         {
-            m_vpoint1 = vanishings[0];
-            m_vpoint2 = vanishings[2];
-            m_vpoint3 = vanishings[1];
+            std::swap(m_vpoint2, m_vpoint3);
         }
         else // if(dist2 <= std::min(dist0, dist1))
         {
-            m_vpoint1 = vanishings[0];
-            m_vpoint2 = vanishings[1];
-            m_vpoint3 = vanishings[2];
+            std::swap(m_vpoint3, m_vpoint3);
         }
 
         // swap vanishing points if necessary, the correct order 
@@ -97,6 +132,24 @@ public:
     // register function
     void registerCascade(SelectableGroup &selectables)
     {
+        // register vanishing points
+        if(m_vpoint1)
+        {
+            selectables.registerObject(this, m_vpoint1);
+            m_vpoint1->registerCascade(selectables);
+        }
+        if(m_vpoint2)
+        {
+            selectables.registerObject(this, m_vpoint2);
+            m_vpoint2->registerCascade(selectables);
+        }
+        if(m_vpoint3)
+        {
+            selectables.registerObject(this, m_vpoint3);
+            m_vpoint3->registerCascade(selectables);
+        }
+
+        // register handle points
         selectables.registerObject(this, &m_handles[0]);
         selectables.registerObject(this, &m_handles[1]);
         selectables.registerObject(this, &m_handles[2]);
@@ -115,15 +168,15 @@ public:
     // render function
     void render(IplImage *temp, const CvScalar &col, const int thickness) const
     {
+        if(m_vpoint1) m_vpoint1->render(temp, CV_RGB(255, 0, 0));
+        if(m_vpoint2) m_vpoint2->render(temp, CV_RGB(0, 255, 0));
+        if(m_vpoint3) m_vpoint3->render(temp, CV_RGB(0, 0, 255));
+
         // compute corners of the wall
         if(m_vpoint1 && m_vpoint2 && m_vpoint3)
         {
             Point2D corners[4];
             computeCorners(corners, *m_vpoint1, *m_vpoint2);
-            //intersectInfiniteLines(LineSegment(m_vpoint1->point(), m_handles[0]), LineSegment(m_vpoint2->point(), m_handles[1]), corners[0]);
-            //intersectInfiniteLines(LineSegment(m_vpoint1->point(), m_handles[2]), LineSegment(m_vpoint2->point(), m_handles[1]), corners[1]);
-            //intersectInfiniteLines(LineSegment(m_vpoint1->point(), m_handles[2]), LineSegment(m_vpoint2->point(), m_handles[3]), corners[2]);
-            //intersectInfiniteLines(LineSegment(m_vpoint1->point(), m_handles[0]), LineSegment(m_vpoint2->point(), m_handles[3]), corners[3]);
 
             // draw wall edges
             cvLine(temp, cvPoint(corners[0]), cvPoint(corners[1]), col, thickness);
@@ -148,33 +201,61 @@ public:
     // output function
     void save(std::ofstream &out) const
     {
-        out << 4 << std::endl;
-        out << m_handles[0] << std::endl;
-        out << m_handles[1] << std::endl;
-        out << m_handles[2] << std::endl;
-        out << m_handles[3] << std::endl;
+        // save vanishing points
+        out << numberOfVanishingPoints() << std::endl;
+        if(m_vpoint1) m_vpoint1->save(out);
+        if(m_vpoint2) m_vpoint2->save(out);
+        if(m_vpoint3) m_vpoint3->save(out);
 
-        // save wall corners
-        Point2D corners[4];
-        computeCorners(corners, *m_vpoint1, *m_vpoint2);
-        //intersectInfiniteLines(LineSegment(m_vpoint1->point(), m_handles[0]), LineSegment(m_vpoint2->point(), m_handles[1]), corners[0]);
-        //intersectInfiniteLines(LineSegment(m_vpoint1->point(), m_handles[2]), LineSegment(m_vpoint2->point(), m_handles[1]), corners[1]);
-        //intersectInfiniteLines(LineSegment(m_vpoint1->point(), m_handles[2]), LineSegment(m_vpoint2->point(), m_handles[3]), corners[2]);
-        //intersectInfiniteLines(LineSegment(m_vpoint1->point(), m_handles[0]), LineSegment(m_vpoint2->point(), m_handles[3]), corners[3]);
+        if(initialisedEnoughVanishingPoints())
+        {
+            // save handle points
+            out << 4 << std::endl;
+            out << m_handles[0] << std::endl;
+            out << m_handles[1] << std::endl;
+            out << m_handles[2] << std::endl;
+            out << m_handles[3] << std::endl;
 
-        out << 4 << std::endl;
-        out << corners[0] << std::endl;
-        out << corners[1] << std::endl;
-        out << corners[2] << std::endl;
-        out << corners[3] << std::endl;
+            // save wall corners
+            Point2D corners[4];
+            computeCorners(corners, *m_vpoint1, *m_vpoint2);
+            out << 4 << std::endl;
+            out << corners[0] << std::endl;
+            out << corners[1] << std::endl;
+            out << corners[2] << std::endl;
+            out << corners[3] << std::endl;
+        }
+        else
+        {
+            out << 0 << std::endl;
+        }
     }
 
     // input function
-    bool load(std::ifstream &in)
+    void load(std::ifstream &in)
     {
+        // load vanishing points
+        int nvanishings = 0;
+        in >> nvanishings;
+        if(nvanishings > 0)
+        {
+            m_vpoint1 = new VanishingPoint();
+            m_vpoint1->load(in);
+        }
+        if(nvanishings > 1)
+        {
+            m_vpoint2 = new VanishingPoint();
+            m_vpoint2->load(in);
+        }
+        if(nvanishings > 2)
+        {
+            m_vpoint3 = new VanishingPoint();
+            m_vpoint3->load(in);
+        }
+
+        // load handles
         int nhandles = 0;
         in >> nhandles;
-
         if(nhandles==4)
         {
             for(int i = 0; i < nhandles; i++)
@@ -183,18 +264,14 @@ public:
                 in >> temp;
                 m_handles[i].set(temp);
             }
-
-            return true;
         }
-
-        return false;
     }
 
 private:
-    Point2D *m_handles;
     VanishingPoint *m_vpoint1;
     VanishingPoint *m_vpoint2;
     VanishingPoint *m_vpoint3;
+    Point2D *m_handles;
 };
 
 
