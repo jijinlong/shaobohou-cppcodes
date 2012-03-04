bb1357f4a7513919f74fcd1c36089aa376fd0e6d
shaobohou <shaobohou@gmail.com>
2012-03-04 16:39:04 +0000

    removed obsolete codes

--- libs/linearalgebra/tnt/jama_qr.h
+++ libs/linearalgebra/tnt/jama_qr.h
@@ -11,26 +11,6 @@ using TNT::Array2D;
 
 namespace JAMA
 {
-
-/**
-	@returns hypotenuse of real (non-complex) scalars a and b by 
-	avoiding underflow/overflow
-	using (a * sqrt( 1 + (b/a) * (b/a))), rather than
-	sqrt(a*a + b*b).
-*/
-template <class Real>
-Real hypot(const Real &a, const Real &b)
-{
-	
-	if (a== 0)
-		return abs(b);
-	else
-	{
-		Real c = b/a;
-		return fabs(a) * sqrt(1 + c*c);
-	}
-}
-
 /**
 <p>
 	Classical QR Decompisition:
